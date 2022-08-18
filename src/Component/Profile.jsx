0e3efb5df616a45c624dfb0530bb09dab1f2080e
LeonG11 <kkacanov198@gmail.com>
2022-08-18 08:06:00 +0300

    new init

--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -1,24 +1,20 @@
 import React from "react";
+import CreateIcon from '@mui/icons-material/Create';
+import DataPicker from './DataPicker';
 
 export default function Profile() {
   return (
-    <div>
-      <div>
-        <h2>Профиль</h2>
-        <button></button>
+    <div className="profile">
+      <div className="profile__head">
+        <h2 className="profile__bold">Профиль</h2>
+        <button className="profile__btn"><CreateIcon/></button>
       </div>
-      <div>
-        <img src="" alt="" />
-        <div></div>
-        <div></div>
+      <div className="profile__img">
+        <img src="./assets/espresso.png" alt="" />
+        <div className="profile__name">Kirill Kachanov</div>
+        <div className="profile__status">Advanced</div>
       </div>
-      <div>
-        <div>
-          <h1></h1>
-          <button></button>
-          <button></button>
-        </div>
-      </div>
+    <DataPicker/>
     </div>
   );
 }
